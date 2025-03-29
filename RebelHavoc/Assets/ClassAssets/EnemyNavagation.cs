7a5efef1a180ef473003f5e2fd022564497a8549
legoman1-dot <myles.chr@gmail.com>
2025-03-28 21:10:57 -0400

    Add files via upload

---
+++ RebelHavoc/Assets/ClassAssets/EnemyNavagation.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Platformer397
+{
+    public class EnemyNavagation : MonoBehaviour, IObserver
+    {
+        private NavMeshAgent agent;
+        [SerializeField] private PlayerController player;
+        [SerializeField] private List<Transform> waypoints = new List<Transform>();
+        [SerializeField] private float distanceThreshold = 1.0f;
+        private int index = 0;
+        private Vector3 destination;
+        
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            destination = waypoints[index].position;
+            player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        }
+
+        
+
+        private void Start()
+        {
+            agent.destination = destination;
+           
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(destination, transform.position) < distanceThreshold)
+            {
+                index = (index + 1) % waypoints.Count;
+                destination = waypoints[index].position;
+                agent.destination = destination;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (waypoints.Count > 0)
+            {
+                Gizmos.color = Color.red;
+                for (int i = 0; i < waypoints.Count; i++)
+                {
+                    Gizmos.DrawSphere(waypoints[i].position, distanceThreshold);
+                    if (i > 0)
+                    {
+                        Gizmos.DrawLine(waypoints[i - 1].position, waypoints[i].position);
+                    }
+                }
+            }
+        }
+
+        public void OnNotify()
+        {
+            Debug.Log($"Notified by the Subject");
+        }
+    }
+}
+
